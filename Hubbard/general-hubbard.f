f76e290d75b54d671268761d3fab12212b593dfa
charm-quark13 <epluhar3@gmail.com>
2018-10-17 14:16:17 -0500

    Calculated B_transverse and B_XC for 3 and 4 point lattices.

--- Hubbard/general-hubbard.f
+++ Hubbard/general-hubbard.f
@@ -8,12 +8,15 @@ C  as portability of the code to future programs.
 
       integer :: i,j,k,iter,it
 
-      real(8) :: ftol,fret,x,v(dim*2),dens(dim*2)
+      real(8) :: ftol,fret,x
       real(8) :: Bx(sites), By(sites), Bz(sites)
-      real(8) :: txc1,txc2
-      real(8) :: vstart(dim*2),vhxc(2),bxc(intd)
+      real(8) :: U0,U1
+      real(8) :: vhxc(sites),bxc(intd),
+     &           vstart(dim*2),v(dim*2),dens(dim*2),vprev(dim*2)
       complex(8) :: htest(intd,intd)
-      real(8) :: tau1(3),tau2(3)
+      real(8) :: tau(3),tx(sites),ty(sites),tz(sites),
+     &           bt(sites),btx(sites),bty(sites),btz(sites),
+     &           bmet
 
       write(matrix,'(a, i3, a)') '(', dim, 'f14.8)'
       write(matprint,'(a, i3, a)') '(', sites, 'e16.6)'
@@ -23,35 +26,25 @@ C  as portability of the code to future programs.
 
       call Pauli(sig)
 
-      write(*,*)'Is this a restart of a previous calculation?'
-      write(*,*)'0 == No ... 1 == Yes'
-      read(*,*) rest
-
-      write(*,*)'Should a torque correction be made?'
-      write(*,*)'0 == No ... 1 == Yes'
-      read(*,*) corr
-
-      write(*,*)'XC=1: Slater'
-      write(*,*)'XC=2: OEP'
-      write(*,*)'XC=3: BALDA (U1=0)'
-      read(*,*)XC
-
       v = 0.d0
       Bx = 0.d0
       by = 0.d0
       bz = 0.d0
 
       v(1) = 1.d0
-      v(3) = -1.d0
+      v(4) = -1.d0
 
       Bx(1) = -0.5d0
       Bx(2) = 1.0d0
       Bx(3) = .5d0
+      Bx(4) = -1.d0
+
 !      Bx = 0.d0
 !      By(2) = .75d0
       Bz(1) = -1.0d0
       Bz(2) = 1.0d0
       Bz(3) = -1.d0
+      Bz(4) = 1.d0
 
 ***************************************************************************
 ***   Setting the initial potentials which will generate our target density.
@@ -74,22 +67,13 @@ C  as portability of the code to future programs.
 ***************************************************************************
 ***   Solving the initial Schrodinger equation for our system via hbuild.
 ***************************************************************************
-      do it=1,100
+      do it=18,18
 
-!        do i=1,sites
-!          v(1) = 1.d0
-!          if (i.ne.1) then
-!            v(i) = -1.d0
-!          end if
-!        end do
+        v = vstart
 
-!        do i=1,sites
-!          v(sites+i) = Bx(i)
-!          v(sites*2+i) = By(i)
-!          v(sites*3+i) = Bz(i)
-!        end do
+        write(*,*) it
 
-        U0 = it/10.d0
+        U0 = dble(it)/10.d0
         U1 = U0/dsqrt(2.d0)
         ntarget = 0.d0
 
@@ -101,7 +85,16 @@ C  as portability of the code to future programs.
 ***   subroutine.
 ***************************************************************************
 !        number = 0
+        !if (it.gt.1) v=vprev
+
         call frprmn(v,dim*2,ftol,iter,fret)
+
+        if (iter.eq.1) then
+          v = vstart
+          call frprmn(v,dim*2,ftol,iter,fret)
+        end if
+
+        vprev = v
 
         write(*,*) '*********************************'
         write(*,*) iter, ftol, fret
@@ -115,43 +108,24 @@ C  as portability of the code to future programs.
         write(*,*) '^^^^^ dens ^^^^^'
         write(*,vector) ntarget
         write(*,*) '^^^^^ n_target ^^^^^'
-!
-!        vhxc = 0.d0
-!        do i=1,2
-!          vhxc(i) = v(i) - vstart(i)
-!        end do
-!
-!        bxc = 0.d0
-!        do i=1,6
-!          k=i+2
-!          bxc(i) = v(k) - vstart(k)
-!        end do
-!
-!        tau1(1) = dens(5)*bxc(5) - dens(7)*bxc(3)
-!        tau1(2) = -(dens(3)*bxc(5)-dens(7)*bxc(1))
-!        tau1(3) = dens(3)*bxc(3) - dens(5)*bxc(1)
-!
-!        tau2(1) = dens(6)*bxc(6) - dens(8)*bxc(4)
-!        tau2(2) = -(dens(4)*bxc(6)-dens(8)*bxc(2))
-!        tau2(3) = dens(4)*bxc(4) - dens(6)*bxc(2)
-!
-!        txc1 = 0.d0
-!        txc2 = 0.d0
-!        do i=1,3
-!          txc1 = txc1 + tau1(i)*tau1(i)
-!          txc2 = txc2 + tau2(i)*tau2(i)
-!        end do
-!
-!        txc1 = dsqrt(txc1)
-!        txc2 = dsqrt(txc2)
-!
-!        open(100,file='xc-torque-3pt.txt')
-!
-!        if (it.eq.1) then
-!          write(100,*) 'U','xc torque 1', 'xc torque 2'
-!        end if
-!        write(100,*) u0,txc1,-txc2
-!        write(*,*) '*** end of loop ***'
+
+        vhxc = 0.d0
+        do i=1,sites
+          vhxc(i) = v(i) - vstart(i)
+        end do
+
+        call tcalc(dens,v,vstart,tau,tx,ty,tz)
+
+        call btrans(dens,v,vstart,bt,btx,bty,btz)
+
+        call metric(bmet,btx,bty,btz,v,vstart)
+
+        open(100,file='metric-4pt.txt')
+
+        !write(100,*) u0,bmet!,fret,iter
+        write(*,*) u0,bmet
+
+        write(*,*) '*** end of loop ***'
       end do
 
       end
